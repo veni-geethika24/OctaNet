293016e6ac326aa8e16cf8bd6082764dc8d04eef
veni geethika <venigeethika@gmail.com>
2025-02-27 14:22:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.getElementById("exploreBtn").addEventListener("click", () => {
+    alert("⌚ Explore the Future of Wearable Technology!");
+});
+
+
+const tryOnButtons = document.querySelectorAll(".try-btn");
+tryOnButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        alert("🕶️ Virtual Try-On Coming Soon... Stay Connected!");
+    });
+});
